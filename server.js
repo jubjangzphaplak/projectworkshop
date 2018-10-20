3dfc34ecfb4795612d9220ee034b1f4b6c1eab40
jubjangzphaplak <42835682+jubjangzphaplak@users.noreply.github.com>
2018-10-21 00:07:28 +0700

    upuser

--- server.js
+++ server.js
@@ -171,6 +171,75 @@ app.get('/users/:pid', function(req,res){
     
 });
 
+//add new user
+app.get('/addnewuser',function(req, res) {
+    res.render('pages/adduser');
+});
+
+app.post('/user/addnewuser', function(req,res){
+    var id = req.body.id;
+    var title = req.body.title;
+    var price = req.body.price;
+    var sql = `INSERT INTO users (id, email, password)
+    VALUES ('${id}', '${email}', '${password}')`;
+    //db.none 
+    console.log('UPDATE:' + sql);
+    db.query(sql)
+    .then(function (data) {
+        console.log('DATA:' + data);
+        res.redirect('/users')
+
+    })
+    .catch(function (error) {
+        console.log('ERROR:' + error);
+    })
+    
+})
+
+// update user
+app.post('/user/update', function(req,res){
+    var id = req.body.id;
+    var title = req.body.title;
+    var price = req.body.price;
+    var sql = `update users 
+    set email =  '${email}' , password = '${password}'
+    where id = '${id}'`;
+
+    //db.none 
+    console.log('UPDATE:' + sql);
+    db.any(sql)
+    .then(function (data) {
+        console.log('DATA:' + data);
+        res.redirect('/users')
+
+    })
+    .catch(function (error) {
+        console.log('ERROR:' + error);
+    })
+    
+})
+
+
+// delete user
+app.get('/user_delete/:id', function(req,res){
+    var id = req.params.id;
+    var sql = 'DELETE FROM users';
+    if(id){
+        sql += ' where id ='+ id; 
+    }
+    db.any(sql)
+    .then(function (data) {
+        console.log('DATA:' + data);
+        res.redirect('/users')
+
+    })
+    .catch(function (error) {
+        console.log('ERROR:' + error);
+    })
+    
+})
+
+
 
 
 
